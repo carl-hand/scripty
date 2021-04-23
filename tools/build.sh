8acd02e4a5de7a2695c1e964c3384e7901197643
carl hand <carl.a.hand92@gmail.com>
2021-04-23 08:21:38 +0100

    adding build script

---
+++ tools/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+build() {
+    echo 'building react'
+
+    rm -rf build/*
+
+    export INLINE_RUNTIME_CHUNK=false
+    export GENERATE_SOURCEMAP=true
+
+    react-scripts build
+
+    cp manifest.json build/manifest.json
+
+    cp src/background.js build
+
+
+    cp src/index.css build
+
+    # sed -i 's#"/static/js/".*$#"/static/js/": "",#g' build/index.html
+    # runtime="/static/js/runtime-main.*.js"
+    # chunk='/static/js/2.*.chunk.js'
+    # main='/static/js/main.*.chunk.js'
+}
+
+build
