04a141354b4db2cc1746ae1a3409a16ea96909ff
carl hand <carl.a.hand92@gmail.com>
2021-04-23 08:23:07 +0100

    adding background script for extension that creates extension in new window when clicked

---
+++ src/background.js
@@ -0,0 +1,26 @@
+/*global chrome*/
+
+let currentWindow;
+chrome.action.onClicked.addListener(async function (tab) {
+  // Create a new window instead of a popup
+  if (currentWindow) {
+    console.log(currentWindow.id);
+    chrome.windows.remove(currentWindow.id);
+  }
+  currentWindow = await chrome.windows.create({
+    url: chrome.runtime.getURL("index.html"),
+    type: "popup",
+    height: 600,
+    width: 450,
+    tabId: tab.id,
+  });
+  console.log(`id: ${tab.id}`);
+});
+
+// background.js
+chrome.runtime.onConnect.addListener(function (externalPort) {
+  externalPort.onDisconnect.addListener(function () {
+    console.log("onDisconnect");
+    // Do stuff that should happen when popup window closes here
+  });
+});
