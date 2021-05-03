2686892600afabc42dfaa07ad858e47f35dba8bc
carl hand <carl.a.hand92@gmail.com>
2021-05-03 14:32:11 +0100

    added comment to sendMessage method

--- src/chrome/api.js
+++ src/chrome/api.js
@@ -46,6 +46,16 @@ export function addMessageListener(getSelectedElements, addEntry) {
   });
 }
 
+/**
+ * Sends a single message to the content script(s) in the specified tab,
+ * with an optional callback to run when a response is sent back.
+ * Calling this method results in the runtime.onMessage event being fired in each
+ * content script running in the specified tab for the current extension
+ *
+ * @param {*} id this is the id of the currently active tab
+ * @param {*} message the message we are sending
+ * @param {*} callback the method to execute when a response is sent back
+ */
 export function sendMessage(id, message, callback) {
   chrome.tabs.sendMessage(id, message, function (response) {
     if (typeof callback === 'function') {
